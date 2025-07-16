1bad8bf05c0795a3a36db792725581c2a435af2b
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-07-16 16:56:19 -0300

    Project created

---
+++ src/test/java/com/rodrigo134/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo134.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

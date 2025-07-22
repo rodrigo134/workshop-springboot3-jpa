d31c541c8906238eb54a76f40c7bad3f8498fff8
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-07-22 10:22:52 -0300

    User entity and resource

---
+++ src/main/java/com/rodrigo134/myproject/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.rodrigo134.myproject.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigo134.myproject.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "maria", "maria@gmail", "999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+
+}

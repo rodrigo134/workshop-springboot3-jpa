90242e068d478016dbf1a9d46699f3c8acf12b7c
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-07-27 17:38:37 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/rodrigo134/myproject/repositories/OrderItemRepository.java
+++ src/main/java/com/rodrigo134/myproject/repositories/OrderItemRepository.java
@@ -3,6 +3,7 @@ package com.rodrigo134.myproject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rodrigo134.myproject.entities.OrderItem;
+import com.rodrigo134.myproject.entities.pk.OrderItemPk;
 
 
 //JPa ja é registrado, nao precisa anotação @Repository
